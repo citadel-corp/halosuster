57521f60848dad6183a597b5ead7d01c396acc61
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2024-05-14 00:46:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# citadel-corp/go-project-template
+
+I'm Commander Shepard, and This Is My Favorite 'Templates for [citadel corp][citadel-corp]'s go projects' on the Citadel
+
+🔍 Tested with - [this][test-url].
+
+📝 Documentation - [this][documentation-url].
+
+🎵 Songs to test by - [playlist](https://open.spotify.com/album/1oVSp3g7ULNAHzFtdBvHEd?si=IVw3cdo6RUKDOdb1gYCJKQ).
+
+## Getting Started
+
+These instructions will give you a copy of the project up and running on
+your local machine for development and testing purposes. See deployment
+for notes on deploying the project on a live system.
+
+### Prerequisites
+
+List of prerequisites
+- This
+- That
+- This too
+
+### Migrate the database
+
+Steps to migrate the database.
+```
+$ run this
+```
+
+### Running the service
+
+Steps to run the service.
+```
+$ run this
+```
+
+## Endpoints
+
+Endpoints (if available).
+
+- Service
+    - Create - `POST /v1/service`
+    - Get - `GET /v1/service`
+    - Update - `PATCH /v1/service`
+    - Delete - `DELETE /v1/service`
+
+## Running the tests
+
+Steps to run the tests.
+
+## Authors
+
+The [Citadel Corp][citadel-corp] team:
+  - [**TheSagab**](https://github.com/TheSagab)
+  - [**Faye**](https://github.com/farolinar)
+
+## License
+
+This project is licensed under the [MIT License][mit-license] - see the [LICENSE][license-file] file for
+details
+
+## Acknowledgments
+
+  - Acknowledge someone/something.
+
+[citadel-corp]: https://github.com/citadel-corp
+[repository]: https://github.com/citadel-corp/go-project-template
+[mit-license]: https://github.com/citadel-corp/go-project-template?tab=MIT-1-ov-file
+[license-file]: https://github.com/citadel-corp/go-project-template/blob/main/LICENSE
+[test-url]: TBA.
+[documentation-url]: TBA.
---
+++ cmd/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/citadel-corp/go-project-template/internal/common/db"
+	"github.com/citadel-corp/go-project-template/internal/common/middleware"
+	"github.com/citadel-corp/go-project-template/internal/image"
+	"github.com/citadel-corp/go-project-template/internal/user"
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = time.RFC3339
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	// Connect to database
+	// env := os.Getenv("ENV")
+	// sslMode := "disable"
+	// if env == "production" {
+	// 	sslMode = "verify-full sslrootcert=ap-southeast-1-bundle.pem"
+	// }
+	// connStr := "postgres://[user]:[password]@[neon_hostname]/[dbname]?sslmode=require"
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?%s",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_PARAMS"))
+	// dbURL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+	// 	os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), sslMode)
+	db, err := db.Connect(connStr)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("Cannot connect to database: %v", err))
+		os.Exit(1)
+	}
+
+	// Create migrations
+	// err = db.UpMigration()
+	// if err != nil {
+	// 	log.Error().Msg(fmt.Sprintf("Up migration failed: %v", err))
+	// 	os.Exit(1)
+	// }
+
+	// initialize user domain
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHandler(userService)
+
+	// initialize image domain
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("ap-southeast-1"),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("S3_ID"), os.Getenv("S3_SECRET_KEY"), ""),
+	})
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("Cannot create AWS session: %v", err))
+		os.Exit(1)
+	}
+	imageService := image.NewService(sess)
+	imageHandler := image.NewHandler(imageService)
+
+	r := mux.NewRouter()
+	r.Use(middleware.Logging)
+	r.Use(middleware.PanicRecoverer)
+	v1 := r.PathPrefix("/v1").Subrouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "Service ready")
+	})
+
+	// user routes
+	ur := v1.PathPrefix("/user").Subrouter()
+	ur.HandleFunc("/register", userHandler.CreateUser).Methods(http.MethodPost)
+	ur.HandleFunc("/login", userHandler.Login).Methods(http.MethodPost)
+
+	// image routes
+	ir := v1.PathPrefix("/image").Subrouter()
+	ir.HandleFunc("", middleware.Authorized(imageHandler.UploadToS3)).Methods(http.MethodPost)
+
+	httpServer := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
+	}
+
+	go func() {
+		log.Info().Msg(fmt.Sprintf("HTTP server listening on %s", httpServer.Addr))
+		if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Error().Msg(fmt.Sprintf("HTTP server error: %v", err))
+		}
+		log.Info().Msg("Stopped serving new connections.")
+	}()
+
+	// Listen for the termination signal
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// Block until termination signal received
+	<-stop
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	log.Info().Msg(fmt.Sprintf("Shutting down HTTP server listening on %s", httpServer.Addr))
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		log.Error().Msg(fmt.Sprintf("HTTP server shutdown error: %v", err))
+	}
+	log.Info().Msg("Shutdown complete.")
+}
